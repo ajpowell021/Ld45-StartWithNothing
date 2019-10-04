498156fb43a178f4158aac67bc1dd16ba99c956f
ajpowell021 <ajpowell021@gmail.com>
2019-10-04 18:17:27 -0400

    Basic setup

---
+++ Ld-45/Assets/Code/ClassManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassManager : MonoBehaviour {
+ 
+    // Instance
+
+    public static ClassManager instance = null;
+    
+    // Objects
+    
+    // Classes
+
+    private InputManager inputManager;
+    
+    // Init
+
+    private void Awake() {
+        setInstance();
+        setObjects();
+        setClasses();
+    }
+    
+    // Private Functions
+
+    private void setInstance() {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+    }
+
+    private void setObjects() {
+        
+    }
+
+    private void setClasses() {
+        inputManager = gameObject.GetComponent<InputManager>();
+    }
+}
