fabc35e39cc6ca736381ec1fbf4b5c123a0fb095
ajpowell021 <ajpowell021@gmail.com>
2019-10-06 00:21:17 -0400

    Add title screen

---
+++ Ld-45/Assets/Code/StartButtonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButtonController : MonoBehaviour {
+    
+    // Public State
+
+    public enum Type {
+        Start,
+        Exit
+    }
+
+    public Type type;
+    
+
+    public Sprite regularSprite;
+    public Sprite blownUpSprite;
+
+    private SpriteRenderer sr;
+
+    private void Awake() {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseOver() {
+        sr.sprite = blownUpSprite;
+    }
+
+    private void OnMouseExit() {
+        sr.sprite = regularSprite;
+    }
+
+    private void OnMouseDown() {
+        if (type == Type.Start) {
+            SceneManager.LoadScene("SampleScene");
+        }
+        else {
+            Application.Quit();
+        }
+    }
+}
